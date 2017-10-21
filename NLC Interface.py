3abd2aaa2d143718f8b2b26feff4ac96c15f7c3b
tristangilbert <32423073+tristangilbert@users.noreply.github.com>
2017-10-21 01:01:50 -0500

    update

---
+++ NLC Interface.py
@@ -0,0 +1,18 @@
+#interact with watson nlc api
+
+import json
+
+from watson_developer_cloud import NaturalLanguageClassifierV1
+
+natural_language_classifier = NaturalLanguageClassifierV1(
+  username="66d51137-b6ef-4e73-9563-b71f3e742e45",
+  password="x760OVu4CbGR")
+
+classifier_id = '1e0d8ex232-nlc-24751'
+
+text = 'I like trump'
+
+#request classification
+classification = natural_language_classifier.classify(classifier_id, text)
+class_name = classification['classes'][0]['class_name']
+confidence = classification['classes'][0]['confidence']
---
+++ TrainingDataCollector.py
@@ -0,0 +1,52 @@
+# Get top comments from top posts on given subreddits
+import praw
+import re
+import csv
+import os
+import os.path
+import collections
+import string
+
+reddit = praw.Reddit(client_id='ZRfPmQnieqTWUw',
+                     client_secret='iUpQc7K0iRw8CZM6tGmPjRBBU4I',
+                     password='timberline3',
+                     user_agent='political leanings by /u/thisaccounttestsbots',
+                     username='thisaccounttestsbots')
+conservative_reddits = ('The_Donald', 'conservative')
+liberal_reddits = ('liberal','SandersForPresident')
+common_words = ('AskReddit', 'funny')
+numPosts = 25
+
+longwords = []
+def writeComments( redditsList, fileName, category ):
+    myFile = fileName
+    if os.path.isfile(myFile):
+        os.remove(myFile)
+    commentsFile = open(myFile, "w", encoding='utf8')
+    for subreddits in redditsList:
+        subreddit = reddit.subreddit(subreddits)
+        hot = subreddit.top(limit=numPosts)
+        for submission in hot:
+            words=[]
+            #submission.comments.replace_more(limit=0)
+            submission.comment_sort = 'new'
+            all_comments = submission.comments.list()
+            for comments in all_comments[0:10]:
+                body = comments.body
+                found_words = re.findall(r'\w+', body)
+                commentString = ''
+                for words in found_words:
+                    commentString += words
+                    commentString += ' '
+                commentString = commentString[0:1000]
+                commentString += ','
+                commentString += category
+                commentString += '\n'
+                commentsFile.write(commentString)
+                print("wrote to file")
+    print("done")
+    commentsFile.close()
+    return 0
+
+conLanguage = writeComments(conservative_reddits, 'conservative_comments.csv', 'conservative')
+libLanguage = writeComments(liberal_reddits, 'liberal_comments.csv', 'liberal')
